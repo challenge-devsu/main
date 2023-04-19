e7362d48527a80c37fdde0e6ea2fbdaed2a12878
Matias Gonzalo Calvo <matias.gonzalo@ext.soprasteria.com>
2023-04-18 21:34:03 -0300

    feat: script prepare env

--- main.sh
+++ main.sh
@@ -2,19 +2,7 @@
 MAX_ATTEMPTS=24
 function install_operator()
 {
-oc apply -f - <<EOF
-apiVersion: operators.coreos.com/v1alpha1
-kind: Subscription
-metadata:
-  name: openshift-gitops-operator
-  namespace: openshift-operators
-spec:
-  channel: stable
-  installPlanApproval: Automatic
-  name: openshift-gitops-operator
-  source: redhat-operators
-  sourceNamespace: openshift-marketplace
-EOF
+  oc apply -f files/installOperatorOpenshiftGitops.yaml
   for ((i=1; i<=MAX_ATTEMPTS; i++)); do
     operator_status=$(oc get pods -n openshift-operators -l control-plane=controller-manager -o jsonpath='{.items[*].status.phase}')
     if [ "$operator_status" == "Running" ]; then
@@ -31,71 +19,12 @@ EOF
 
 function create_argo_things ()
 {
-oc apply -f - <<EOF
-apiVersion: project.openshift.io/v1
-kind: Project
-metadata:
-  name: app-${1}
-EOF
-
-#create appproject
-oc apply -f - <<EOF
-apiVersion: argoproj.io/v1alpha1
-kind: AppProject
-metadata:
-  name: app-${1}
-  namespace: openshift-gitops
-spec:
-  clusterResourceWhitelist:
-  - group: '*'
-    kind: '*'
-  description: app-${1}
-  destinations:
-  - namespace: '*'
-    server: '*'
-  sourceRepos:
-  - '*'
-EOF
-
-oc apply -f - <<EOF
-apiVersion: argoproj.io/v1alpha1
-kind: ApplicationSet
-metadata:
-  name: app-${1}
-  namespace: openshift-gitops
-spec:
-  generators:
-  - clusters: {}
-  template:
-    metadata:
-      name: app-${1}
-    spec:
-      destination:
-        namespace: app-${1}
-        server: https://kubernetes.default.svc
-      ignoreDifferences:
-      - jsonPointers:
-        - /imagePullSecrets
-        - /secrets
-        kind: ServiceAccount
-      project: app-${1}
-      source:
-        helm:
-          valueFiles:
-          - values.yaml
-          - configmap.yaml
-          - secrets.yaml
-        path: devops/${1}
-        repoURL: https://github.com/challenge-devsu/app.git
-        targetRevision: ${2}
-      syncPolicy:
-        automated:
-          prune: true
-          selfHeal: true
-        syncOptions:
-        - Replace=false
-        - PruneLast=true
-EOF
+  ENV=${1}
+  BRANCH=${2}
+  cat files/project.yaml | sed "s/__ENV__/${ENV}/g" | oc apply -f - 
+  cat files/appProject.yaml | sed "s/__ENV__/${ENV}/g" | oc apply -f -
+  cat files/applicationSet.yaml | sed "s/__ENV__/${ENV}/g" | sed "s/__BRANCH__/${BRANCH}/g" | oc apply -f -
+  
 }
 
 install_operator
